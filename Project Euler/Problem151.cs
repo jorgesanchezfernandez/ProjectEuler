59e882dae9ec8c70bb8efc4ac92ea59fecf1ae0f
jfernandez <jfernandez@abcam.com>
2017-08-03 14:00:38 +0100

    My first commit

---
+++ Project Euler/Problem151.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Project_Euler
+{
+    using System;
+
+    class Problem151
+    {
+        static void Main(string[] args)
+        {
+            var totalNumberBranches = 0;
+            var initialNumber = "2345";
+            var equivalency = new Dictionary<string, string> { { "2", "345" }, { "3", "45" }, { "4", "5" }, { "5", " " } };
+
+            // Number of branches found it for [index] matched 
+            var fiveList = new List<int> { 0, 0, 0, 0 };
+            var fourList = new List<int> { 1, 0, 0, 0 };
+            var threeList = new List<int> { 2, 2, 0, 0};
+            var twoList = new List<int>();
+
+            var initialList = new Dictionary<string, List<int>> { { "5", fiveList }, { "4", fourList }, { "3", threeList }, { "2", twoList } };
+
+            //Build the tree
+            //Traverse taken the probability
+            //Check in the same level if the number it was already calculated
+            //Save this whole number of this level in a list.
+
+        }
+    }
+}
---
+++ Project Euler/Utils/NaryTree.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Project_Euler.Utils
+{
+    public delegate void TreeVisitor<T>(T nodeData);
+
+    public class NTree<T>
+    {
+        private T data;
+        private LinkedList<NTree<T>> children;
+
+        public NTree(T data)
+        {
+            this.data = data;
+            children = new LinkedList<NTree<T>>();
+        }
+
+        public void AddChild(T data)
+        {
+            children.AddFirst(new NTree<T>(data));
+        }
+
+        public NTree<T> GetChild(int i)
+        {
+            foreach (NTree<T> n in children)
+                if (--i == 0)
+                    return n;
+            return null;
+        }
+
+        public void Traverse(NTree<T> node, TreeVisitor<T> visitor)
+        {
+            visitor(node.data);
+            foreach (NTree<T> kid in node.children)
+                Traverse(kid, visitor);
+        }
+
+        public int TraverseTakenProbabilities(NTree<T> node, TreeVisitor<T> visitor)
+        {
+            var probabilityInLevel = 1 / node.children.Count;
+            
+            foreach (NTree<T> kid in node.children)
+            {
+                var probabilityLevelMinus1 = TraverseTakenProbabilities(kid, visitor);
+                probabilityInLevel = probabilityInLevel * probabilityLevelMinus1;
+            }
+
+            return probabilityInLevel;
+        }
+    }
+}
