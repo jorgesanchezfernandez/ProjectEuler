0699d1b4673268a439655b9978727028c2a59d98
jfernandez <jfernandez@abcam.com>
2017-08-04 14:35:35 +0100

    Nuevo avance

--- Project Euler/Problem151.cs
+++ Project Euler/Problem151.cs
@@ -9,19 +9,43 @@ namespace Project_Euler
     {
         static void Main(string[] args)
         {
-            var probabilidadParaCero = 0;
-            var probabilidadParaUno = 0;
-            var probalidadParaDos = 0;
-            var probalidadParaTres = 0;
+            var probabilidadesPara = new List<int> { 0, 0, 0, 0 };
+            var sumatorioDeLaprobabilidadDeLaRama = 0;
+            
             var numeroTotalDeRamas = 0;
             var arbol = new NTree<List<int>>(new List<int> { 2, 3, 4, 5 });
+            var numeroSolitariosEncontrados = 0;
 
             //Build the tree
-            for ( var i = 0; i < arbol.GetNode().Count; i++ )
+            for (var i = arbol.GetNode().Count; i >= 0; i--)
             {
-                var probabilidadDelNivel = 1 / arbol.GetNode().Count;
+                var sumatorioDeProbabilidadDesdeNodoHastaHoja = 0; // incompleto
+
+                if (arbol.GetNode().Count == 1)
+                {
+                    if (arbol.GetNode()[i] == 5)
+                    {
+                        probabilidadesPara[numeroSolitariosEncontrados] += sumatorioDeLaprobabilidadDeLaRama;
+                    }
+                    else
+                    {
+                        numeroSolitariosEncontrados++;
+                    }
+                }
+                else
+                {
+                    var probabilidadDelNivel = 1 / arbol.GetNode().Count;
+                    sumatorioDeLaprobabilidadDeLaRama = sumatorioDeLaprobabilidadDeLaRama * probabilidadDelNivel;
+                }
 
                 var nuevoNodo = new List<int>();
+                for (var j = 0; j < arbol.GetNode().Count; j++ )
+                {
+                    if (j != i)
+                    {
+                        nuevoNodo.Add(arbol.GetNode()[j]);
+                    }
+                }
                 if (arbol.GetNode()[i] == 2)
                 {
                     nuevoNodo.Add(3);
@@ -37,26 +61,22 @@ namespace Project_Euler
                 {
                     nuevoNodo.Add(5);
                 }
+                nuevoNodo.Sort(); // Importante para no repetir las busquedas
 
-                for (var j = 0; j < arbol.GetNode().Count; j++ )
-                {
-                    if (j != i)
-                    {
-                        nuevoNodo.Add(arbol.GetNode()[j]);
-                    }
-                }
                 if (arbol.GetNode()[i] != arbol.GetNode()[i + 1])
                 {
-                    calculoNuevoNivel(arbol);
+                    calculoNuevoNivel(arbol, sumatorioDeLaprobabilidadDeLaRama);
                 }else
                 {
                     //La misma probabilidad anterior
                 }
+
+
             }
             //Traverse taken the probability
             //Check in the same level if the number it was already calculated
             //Save this whole number of this level in a list.
-            var probabilidadTotal = probabilidadParaCero + probabilidadParaUno + probalidadParaDos + probalidadParaTres;
+            var probabilidadTotal = probabilidadesPara[0] + probabilidadesPara[1] + probabilidadesPara[2] + probabilidadesPara[3];
 
             Console.WriteLine(probabilidadTotal / numeroTotalDeRamas);
             Console.ReadKey();
